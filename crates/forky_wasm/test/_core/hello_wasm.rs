acb508289f2218795f4c0b2c76c368155298678b
Peter Hayman <peteyhayman@gmail.com>
2022-11-09 15:51:20 +1100

    remove hello wasm world

--- crates/forky_wasm/test/_core/hello_wasm.rs
+++ crates/forky_wasm/test/_core/hello_wasm.rs
@@ -22,25 +22,21 @@ sweet! {
 
 	let mut engine = WasmEngine::new();
 	let mut buf = read_wasm_bytes("simple").unwrap();
+	let mut instance = engine.instantiate().add_import(
+		"host", "hello",|mut caller, param:i32| {
+			let data = caller.host_data_mut();
+			*data = *data + param as u32;
+		}
+	).build(&buf[..]);
 
-	test "buffer"{
-		engine.run_compiled(&buf[..]);
-
-	}
-	test only "buffer"{
-		let mut instance = engine.instantiate().add_import(
-			"host", "hello",|mut caller, param:i32| {
-				let data = caller.host_data_mut();
-				*data = *data + param as u32;
-			}
-		).build(&buf[..]);
-
-		let hello = instance.get_export::<(),()>("hello");
-		hello.call(&mut instance.store,());
+	test "exports" {
 		let add = instance.get_export::<(u64,u64),u64>("add");
 		let result = add.call(&mut instance.store,(1,2)).unwrap();
 		expect(result).to_be(3)?;
-
+	}
+	test "imports" {
+		let hello = instance.get_export::<(),()>("hello");
+		hello.call(&mut instance.store,());
 		let state = instance.store.state();
 		expect(*state).to_be(54)?;
 
