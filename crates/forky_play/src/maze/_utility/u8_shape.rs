3288cf6683ccad01306478e8b5c29b7475998930
Peter Hayman <peteyhayman@gmail.com>
2022-11-16 04:53:57 +1100

    move spawners

--- crates/forky_play/src/maze/_utility/u8_shape.rs
+++ crates/forky_play/src/maze/_utility/u8_shape.rs
@@ -1,24 +1,22 @@
+pub const NONE: u8 = 0;
+pub const VERTICAL: u8 = 1;
+pub const HORIZONTAL: u8 = 2;
+pub const CROSS: u8 = 3;
 
+pub const TOP_LEFT: u8 = 4;
+pub const TOP_RIGHT: u8 = 5;
+pub const BOTTOM_RIGHT: u8 = 6;
+pub const BOTTOM_LEFT: u8 = 7;
 
-pub const NONE:u8 = 0;
-pub const VERTICAL:u8 = 1;
-pub const HORIZONTAL:u8 = 2;
-pub const CROSS:u8 = 3;
+pub const TOP_TEE: u8 = 8;
+pub const BOTTOM_TEE: u8 = 9;
+pub const LEFT_TEE: u8 = 10;
+pub const RIGHT_TEE: u8 = 11;
 
-pub const TOP_LEFT:u8 = 4;
-pub const TOP_RIGHT:u8 = 5;
-pub const BOTTOM_RIGHT:u8 = 6;
-pub const BOTTOM_LEFT:u8 = 7;
-
-pub const TOP_TEE:u8 = 8;
-pub const BOTTOM_TEE:u8 = 9;
-pub const LEFT_TEE:u8 = 10;
-pub const RIGHT_TEE:u8 = 11;
-
-pub const VERTICAL_TOP:u8 = 12;
-pub const VERTICAL_BOTTOM:u8 = 13;
-pub const HORIZONTAL_LEFT:u8 = 14;
-pub const HORIZONTAL_RIGHT:u8 = 15;
+pub const VERTICAL_TOP: u8 = 12;
+pub const VERTICAL_BOTTOM: u8 = 13;
+pub const HORIZONTAL_LEFT: u8 = 14;
+pub const HORIZONTAL_RIGHT: u8 = 15;
 
 pub const DIAG_TL_BR: u8 = 16;
 pub const DIAG_TR_BL: u8 = 17;
