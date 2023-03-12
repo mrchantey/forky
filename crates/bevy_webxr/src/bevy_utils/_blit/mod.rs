bf27682e183066a5902c42824e49da46fe20fd65
Peter Hayman <peteyhayman@gmail.com>
2023-03-12 20:33:55 +1100

    remove: blit approach

--- crates/bevy_webxr/src/bevy_utils/_blit/mod.rs
+++
@@ -1,8 +0,0 @@
-mod blit_node;
-pub use self::blit_node::*;
-mod blit_pipeline;
-pub use self::blit_pipeline::*;
-mod blit_src;
-pub use self::blit_src::*;
-mod blit_target;
-pub use self::blit_target::*;
--- crates/bevy_webxr/src/bevy_utils/_graph/mod.rs
+++
@@ -1,6 +0,0 @@
-mod clear_node;
-pub use self::clear_node::*;
-mod clear_src_node;
-pub use self::clear_src_node::*;
-mod insert_node;
-pub use self::insert_node::*;
