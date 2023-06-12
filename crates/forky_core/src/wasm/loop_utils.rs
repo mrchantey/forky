cdbf442f6ab42828b2d01838ea6122cae5145a7c
Peter Hayman <peteyhayman@gmail.com>
2023-06-12 16:21:06 +1000

    feat: wasm loop utils

---
+++ crates/forky_core/src/wasm/loop_utils.rs
@@ -0,0 +1,21 @@
+use js_sys::Promise;
+use wasm_bindgen_futures::JsFuture;
+
+
+pub async fn loop_forever() -> ! {
+	loop {
+		wait_for_millis(1000).await;
+	}
+}
+
+pub async fn wait_for_millis(millis: i32) {
+	let promise = Promise::new(&mut |resolve, _reject| {
+		web_sys::window()
+			.unwrap()
+			.set_timeout_with_callback_and_timeout_and_arguments_0(
+				&resolve, millis,
+			)
+			.expect("Should register `setTimeout`.");
+	});
+	JsFuture::from(promise).await.unwrap();
+}
--- crates/forky_core/src/wasm/mod.rs
+++ crates/forky_core/src/wasm/mod.rs
@@ -2,5 +2,7 @@ mod animation;
 pub use self::animation::*;
 mod elements;
 pub use self::elements::*;
+mod loop_utils;
+pub use self::loop_utils::*;
 mod utils;
 pub use self::utils::*;
