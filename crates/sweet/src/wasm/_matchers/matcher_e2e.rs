92fa641b804d4fc21568c1bdf480e84268fae51f
Peter Hayman <25616826+mrchantey@users.noreply.github.com>
2023-08-08 16:44:06 +1000

    feat: e2e demo test

--- crates/sweet/src/wasm/_matchers/matcher_e2e.rs
+++ crates/sweet/src/wasm/_matchers/matcher_e2e.rs
@@ -1,9 +1,10 @@
+use anyhow::Result;
 use forky_web::*;
 use web_sys::Document;
 use web_sys::HtmlIFrameElement;
-use anyhow::Result;
+use web_sys::Window;
 
-const NO_IFRAME:&str = r#"
+const NO_IFRAME: &str = r#"
 iframe only available in e2e tests:
 
 it e2e "works" {
@@ -12,13 +13,13 @@ it e2e "works" {
 "#;
 
 
-pub async fn visit(url: &str)->Result<()> {
-	match Document::x_query_selector::<HtmlIFrameElement>("iframe"){
-		None=>Err(anyhow::anyhow!(NO_IFRAME)),
-		Some(iframe)=>{
+pub async fn visit(url: &str) -> Result<HtmlIFrameElement> {
+	match Document::x_query_selector::<HtmlIFrameElement>("iframe") {
+		None => Err(anyhow::anyhow!(NO_IFRAME)),
+		Some(iframe) => {
 			iframe.set_src(url);
 			//TODO await
-			Ok(())
+			Ok(iframe)
 		}
 	}
 }
--- crates/sweet/test/wasm/end_to_end.rs
+++ crates/sweet/test/wasm/end_to_end.rs
@@ -1,9 +1,18 @@
+use forky_core::*;
 use sweet::*;
 
 sweet! {
 	it e2e "works" {
-		let _page = visit("https://example.com/").await?;
+		let page = visit("?m=%21").await?;
+		// log!("{inner_html}");
+		poll_ok(move ||{
+			if let Some(body) = page.content_window().unwrap().document().unwrap().body(){
+				let inner_text = body.inner_text();
+				expect(inner_text.clone().as_str()).to_contain("🤘 sweet as! 🤘")
+			}else{
+			expect(true).to_be_false()
+			}
+		}).await?;
 		// expect(true).to_be_false()?;
-
 	}
 }
