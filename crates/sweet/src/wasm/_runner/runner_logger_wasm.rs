03acc902289ebcc11c4d854b78aa75d8627daa29
Peter Hayman <peteyhayman@gmail.com>
2023-08-21 13:37:47 +0700

    wip: e2e automation

--- crates/sweet/src/wasm/_runner/runner_logger_wasm.rs
+++ crates/sweet/src/wasm/_runner/runner_logger_wasm.rs
@@ -1,26 +1,33 @@
-use std::time::Duration;
 use crate::*;
 use forky_core::*;
 use forky_web::*;
+use std::time::Duration;
 use web_sys::console;
 
-pub struct RunnerLoggerWasm{
-start_time:f64
+pub struct RunnerLoggerWasm {
+	start_time: f64,
+	logger_export: LoggerExport,
 }
 
 
 impl RunnerLogger for RunnerLoggerWasm {
-	fn start(config: &TestRunnerConfig) ->Self{
+	fn start(config: &TestRunnerConfig) -> Self {
 		console::clear();
 		let intro = Self::pretty_print_intro(&config);
 		log!("{intro}");
+		let logger_export = LoggerExport::init();
+		logger_export.push(&intro);
 		let start_time = performance_now();
-		Self{start_time}
+		Self {
+			start_time,
+			logger_export,
+		}
 	}
-	fn end(self,results: &ResultSummary) {
+	fn end(self, results: &ResultSummary) {
 		let duration =
-				Duration::from_millis((performance_now() - self.start_time) as u64);
+			Duration::from_millis((performance_now() - self.start_time) as u64);
 		let summary = Self::pretty_print_summary(&results, duration);
 		log!("{summary}");
+		self.logger_export.push(&summary);
 	}
 }
--- crates/sweet/src/wasm/_runner/suite_logger_wasm.rs
+++ crates/sweet/src/wasm/_runner/suite_logger_wasm.rs
@@ -2,7 +2,9 @@ use crate::*;
 use forky_core::*;
 // use forky_web::LogBuffer;
 
-pub struct SuiteLoggerWasm;
+pub struct SuiteLoggerWasm {
+	pub logger_export: LoggerExport,
+}
 // pub log: String,
 // pub buffer_log: LogBuffer,
 // pub buffer_err: LogBuffer,
@@ -10,9 +12,13 @@ pub struct SuiteLoggerWasm;
 
 //TODO in progress erasable
 impl SuiteLogger for SuiteLoggerWasm {
-	fn on_start(_: String) -> Self { SuiteLoggerWasm }
+	fn on_start(_: String) -> Self {
+		let logger_export = LoggerExport::get_or_init();
+		Self { logger_export }
+	}
 
 	fn on_end(self, end_str: String) {
 		log!("{end_str}");
+		self.logger_export.push(&end_str);
 	}
 }
