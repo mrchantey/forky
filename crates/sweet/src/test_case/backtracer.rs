1ba1c0e6c6fbb2eaec1a85eae9d565907ba5c4c7
Peter Hayman <peteyhayman@gmail.com>
2023-08-29 09:12:21 +0700

    Update backtracer.rs

--- crates/sweet/src/test_case/backtracer.rs
+++ crates/sweet/src/test_case/backtracer.rs
@@ -1,10 +1,11 @@
 use backtrace::Backtrace;
 
+#[cfg(target_os = "linux")]
+const PREFIX_STACK_FRAMES: usize = 1;
+#[cfg(target_os = "macos")]
+const PREFIX_STACK_FRAMES: usize = 2;
 #[cfg(target_os = "windows")]
 const PREFIX_STACK_FRAMES: usize = 6;
-#[cfg(not(target_os = "windows"))]
-// #[cfg(any(target_os = "macos", target_os = "linux"))]
-const PREFIX_STACK_FRAMES: usize = 1;
 // const RUST_INTERNAL_FILE: &str = "library\\core\\src\\ops\\function.rs";
 
 #[cfg(target_arch = "wasm32")]
