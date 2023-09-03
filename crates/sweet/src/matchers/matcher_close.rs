1eeaad991549e4cdf4a0db83fa01acd121f73b0b
Peter Hayman <peteyhayman@gmail.com>
2023-09-03 21:35:31 +0700

    Update matcher_close.rs

--- crates/sweet/src/matchers/matcher_close.rs
+++ crates/sweet/src/matchers/matcher_close.rs
@@ -9,7 +9,11 @@ where
 	U: CloseTo + std::fmt::Debug + Copy,
 {
 	fn to_be_close_to(&self, expected: impl SweetBorrow<U>) -> Result<()> {
-		self.to_be_close_to_with_epsilon(expected, U::default_epsilon())
+		let received = self.value.sweet_borrow();
+		let expected = expected.sweet_borrow();
+		let result = U::is_close(received, expected, U::default_epsilon());
+		let expected = format!("close to {:?}", expected);
+		self.assert_correct_with_received(result, &expected, &received)
 	}
 	fn to_be_close_to_with_epsilon(
 		&self,
