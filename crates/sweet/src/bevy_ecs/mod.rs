d0c65c2d182e3d7f6e16443a4d44691f37bfb8d6
Peter Hayman <25616826+mrchantey@users.noreply.github.com>
2023-09-22 11:40:53 +1000

    feat: bevy_app support

--- crates/sweet/src/bevy_ecs/mod.rs
+++ crates/sweet/src/bevy_ecs/mod.rs
@@ -1,2 +1,4 @@
+mod sweet_into;
+pub use self::sweet_into::*;
 mod world;
 pub use self::world::*;
---
+++ crates/sweet/src/bevy_ecs/sweet_into.rs
@@ -0,0 +1,7 @@
+use crate::*;
+use ::bevy_app::prelude::*;
+use ::bevy_ecs::prelude::*;
+
+
+impl<'a> SweetInto<&'a World> for &'a App
+{ fn sweet_into(self) -> &'a World { &self.world } }
