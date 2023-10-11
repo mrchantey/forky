0f9c48cc2e6d37f0a5c9d640ec4ca65a1806c7cb
Peter Hayman <25616826+mrchantey@users.noreply.github.com>
2023-10-12 01:58:20 +1100

    test: trees

--- crates/gamai/test/common/tree.rs
+++ crates/gamai/test/common/tree.rs
@@ -1,34 +1,34 @@
-// use bevy_app::App;
-// use gamai::*;
-// use sweet::*;
+use bevy_app::App;
+use gamai::*;
+use sweet::*;
 
 
-// #[tree_builder]
-// pub fn MyTree() -> impl AiTree {
-// 	tree! {
-// 		<sequence>
-// 			<node_always_succeed/>
-// 		</sequence>
-// 	}
-// }
+#[tree_builder]
+pub fn MyTree() -> impl AiNode {
+	tree! {
+		<sequence>
+			<node_always_succeed/>
+		</sequence>
+	}
+}
 
 
-// #[sweet_test]
-// pub fn it_works() -> Result<()> {
-// 	let mut app = App::new();
+#[sweet_test]
+pub fn it_works() -> Result<()> {
+	let mut app = App::new();
 
-// 	app.add_plugins(MyTree.plugin());
+	app.add_plugins(AiPlugin::new(MyTree));
 
-// 	let entity = app.world.spawn(MyTree.bundle()).id();
+	let entity = app.world.spawn(AiBundle::new(MyTree)).id();
 
-// 	expect(MyTree.node_state(&app.world, entity))
-// 		.to_be(Some(NodeState::Running))?;
+	expect(MyTree.node_state(&app.world, entity))
+		.to_be(Some(NodeState::Running))?;
 
-// 	app.update();
-// 	app.update();
+	app.update();
+	app.update();
 
-// 	expect(MyTree.node_state(&app.world, entity))
-// 		.to_be(Some(NodeState::Success))?;
+	expect(MyTree.node_state(&app.world, entity))
+		.to_be(Some(NodeState::Success))?;
 
-// 	Ok(())
-// }
+	Ok(())
+}
--- crates/gamai/test/common/utility_tree.rs
+++ crates/gamai/test/common/utility_tree.rs
@@ -1,61 +1,30 @@
-// use bevy_app::App;
-// use gamai::*;
-// use sweet::*;
+use bevy_app::App;
+use gamai::*;
+use sweet::*;
 
 
-// #[tree_builder]
-// pub fn MyTree() -> impl AiTree {
-// 	tree! {
-// 		<first_valid_edge>
-// 			<empty_node edge=edge_always_fail/>
-// 			<empty_node edge=edge_always_pass/>
-// 		</first_valid_edge>
-// 	}
-// }
+#[tree_builder]
+pub fn MyTree() -> impl AiNode {
+	tree! {
+		<first_valid_edge>
+			<empty_node edge=edge_always_fail/>
+			<empty_node edge=edge_always_pass/>
+		</first_valid_edge>
+	}
+}
 
-// #[sweet_test]
-// pub fn it_works() -> Result<()> {
-// 	let mut app = App::new();
+#[sweet_test]
+pub fn it_works() -> Result<()> {
+	let mut app = App::new();
 
-// 	app.add_plugins(MyTree.plugin());
+	app.add_plugins(AiPlugin::new(MyTree));
 
-// 	let entity = app.world.spawn(MyTree.bundle()).id();
-// 	app.update();
+	let entity = app.world.spawn(AiBundle::new(MyTree)).id();
+	app.update();
 
-// 	expect(MyTree.child(0).node_state(&app.world, entity))
-// 		.to_be_none()?;
-// 	expect(MyTree.child(1).node_state(&app.world, entity))
-// 		.to_be(Some(NodeState::Running))?;
+	expect(MyTree.child(0).node_state(&app.world, entity)).to_be_none()?;
+	expect(MyTree.child(1).node_state(&app.world, entity))
+		.to_be(Some(NodeState::Running))?;
 
-// 	Ok(())
-// }
-
-// // sweet! {
-
-// // 	test "utility tree" {
-// // 		let mut world = World::new();
-// // 		let mut schedule = Schedule::default();
-// // 		MyTree::add_systems(&mut schedule);
-// // 		let entity = world.spawn(MyTree::bundle()).id();
-// // 		expect(&world)
-// // 			.to_have_component::<DerefEdgeState<MyTree>>(entity)?;
-// // 		expect(&world)
-// // 			.to_have_component::<DerefNodeState<MyTree>>(entity)?;
-// // 		expect(&world)
-// // 			.to_have_component::<DerefEdgeState<Child0>>(entity)?;
-// // 		expect(&world).not()
-// // 			.to_have_component::<DerefNodeState<Child0>>(entity)?;
-// // 		schedule.run(&mut world);
-// // 		// schedule.run(&mut world);
-// // 		expect(&world)
-// // 			.component::<DerefEdgeState<Child0>>(entity)?
-// // 			.map(|a| a.state)
-// // 			.to_be(EdgeState::Fail)?;
-// // 		expect(&world)
-// // 			.component::<DerefEdgeState<Child1>>(entity)?
-// // 			.map(|a| a.state)
-// // 			.to_be(EdgeState::Pass)?;
-// // 		expect(&world).not().to_have_component::<DerefNodeState<Child0>>(entity)?;
-// // 		expect(&world).to_have_component::<DerefNodeState<Child1>>(entity)?;
-// // 	}
-// // }
+	Ok(())
+}
