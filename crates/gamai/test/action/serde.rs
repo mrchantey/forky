0f68f91c05d8010dac7c5fd0da881e231efd6414
Peter Hayman <peteyhayman@gmail.com>
2024-02-06 10:20:30 +1100

    remove: boxed action graph

--- crates/gamai/test/action/serde.rs
+++ crates/gamai/test/action/serde.rs
@@ -3,21 +3,10 @@ use gamai::prelude::*;
 use sweet::*;
 
 #[sweet_test]
-pub fn boxed() -> Result<()> {
-	let actions1 = test_action_graph_typed();
-	let str1 = serde_json::to_string_pretty(&actions1)?;
-	let actions2 = serde_json::from_str::<BoxedActionGraph>(&str1)?;
-	let str2 = serde_json::to_string_pretty(&actions2)?;
-
-	expect(&str1).to_be(&str2)?;
-
-	Ok(())
-}
-#[sweet_test]
 pub fn typed() -> Result<()> {
 	let actions1 = test_action_graph_typed();
 	let str1 = serde_json::to_string_pretty(&actions1)?;
-	let actions2 = serde_json::from_str::<ActionGraph<BuiltinNode>>(&str1)?;
+	let actions2 = serde_json::from_str::<BehaviorGraph<BuiltinNode>>(&str1)?;
 	let str2 = serde_json::to_string_pretty(&actions2)?;
 
 	expect(&str1).to_be(&str2)?;
