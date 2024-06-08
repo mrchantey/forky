3b0aa4a02257318553d01bf5d24bdb9845c4f18c
Peter Hayman <peteyhayman@gmail.com>
2024-06-09 09:47:39 +1000

    deps: bevy 0.14.0-rc.2

--- crates/forky/forky_bevy/src/systems/exit_system.rs
+++ crates/forky/forky_bevy/src/systems/exit_system.rs
@@ -3,13 +3,13 @@ use bevy::core::FrameCount;
 use bevy::prelude::*;
 
 
-pub fn exit_system(mut exit: EventWriter<AppExit>) { exit.send(AppExit); }
+pub fn exit_system(mut exit: EventWriter<AppExit>) { exit.send(AppExit::Success); }
 pub fn exit_in_frames(
 	count: u32,
 ) -> impl Fn(Res<FrameCount>, EventWriter<AppExit>) {
 	move |frames, mut exit| {
 		if frames.0 >= count - 1 {
-			exit.send(AppExit);
+			exit.send(AppExit::Success);
 		}
 	}
 }
