7c2c2b96473540268a1cc059cdeb9a53ad202a46
Peter Hayman <peteyhayman@gmail.com>
2024-09-16 18:40:41 +1000

    patch

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #![doc = include_str!("../README.md")]
 pub use forky_core as core;
 #[cfg(feature = "fs")]
-pub use forky_fs as net;
+pub use forky_fs as fs;
 #[cfg(feature = "web")]
-pub use forky_web as net;
+pub use forky_web as web;
 #[cfg(feature = "cli")]
-pub use forky_cli as net;
+pub use forky_cli as cli;
 #[cfg(feature = "bevy")]
-pub use forky_bevy as net;
+pub use forky_bevy as bevy;
 
 pub mod prelude {
 	pub use crate::core::prelude::*;
