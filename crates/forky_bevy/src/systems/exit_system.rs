8a415a4a63dda63ad69da84bc8e2d851af6246de
Peter Hayman <peteyhayman@gmail.com>
2024-12-07 18:16:13 +1100

    release: 0.2.0

--- crates/forky_bevy/src/systems/exit_system.rs
+++ crates/forky_bevy/src/systems/exit_system.rs
@@ -2,31 +2,19 @@ use bevy::app::AppExit;
 use bevy::core::FrameCount;
 use bevy::prelude::*;
 
-
-pub fn exit_system(mut exit: EventWriter<AppExit>) { exit.send(AppExit::Success); }
-pub fn exit_in_frames(
-	count: u32,
-) -> impl Fn(Res<FrameCount>, EventWriter<AppExit>) {
-	move |frames, mut exit| {
-		if frames.0 >= count - 1 {
-			exit.send(AppExit::Success);
-		}
-	}
+pub fn exit_system(mut exit: EventWriter<AppExit>) {
+    exit.send(AppExit::Success);
+}
+pub fn exit_in_frames(count: u32) -> impl Fn(Res<FrameCount>, EventWriter<AppExit>) {
+    move |frames, mut exit| {
+        if frames.0 >= count - 1 {
+            exit.send(AppExit::Success);
+        }
+    }
 }
 
-
-pub fn close_on_esc(
-	mut commands: Commands,
-	focused_windows: Query<(Entity, &Window)>,
-	input: Res<ButtonInput<KeyCode>>,
-) {
-	for (window, focus) in focused_windows.iter() {
-		if !focus.focused {
-			continue;
-		}
-
-		if input.just_pressed(KeyCode::Escape) {
-			commands.entity(window).despawn();
-		}
-	}
+pub fn close_on_esc(mut exit: EventWriter<AppExit>, input: Res<ButtonInput<KeyCode>>) {
+    if input.just_pressed(KeyCode::Escape) {
+        exit.send(AppExit::Success);
+    }
 }
